480ca0b1a0e7a23a2ead5b04de753cf157b7fbaf
kydsib <simonas.kersulis@gmail.com>
2023-05-04 15:54:39 +0300

    Testing gitignore

--- src/__tests__/HomePage.test.tsx
+++ src/__tests__/HomePage.test.tsx
@@ -2,11 +2,6 @@ import { render, screen } from '@testing-library/react';
 import Home from '@app/page';
 
 describe('Home', () => {
-  it('should render', () => {
-    render(<Home />);
-    screen.getByText('Apie')
-    screen.findAllByAltText('Podcastas')
-    screen.getByText('Registruotis konsultacijai')
-  });
+  it.todo('Probably should add playwright tests here')
 }
 );
